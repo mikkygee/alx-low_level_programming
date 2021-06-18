1429d66468939e262d3f411c04cff43000524506
mikkygee <kevwedeke@gmail.com>
2021-06-18 13:19:51 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+		putchar(k);
+		putchar('\n');
+
+return (0);
+}
+
