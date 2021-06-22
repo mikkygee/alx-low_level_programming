63e7eaf36b058f0ea7ca2adb6b31bfa86fddf586
mikkygee <kevwedeke@gmail.com>
2021-06-22 13:23:44 -0700

    initial commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 /**
  * main - Entry Point
- * 
+ *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	int k;
-	
+
 	for (k = 'a'; k <= 'z'; k++)
-	{
+
 		_putchar(k);
 		_putchar('\n');
 
-	}
 }
 
