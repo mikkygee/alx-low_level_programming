a0d5ecca81bd2d61085ea9a4dd5d1b1e33a79ade
mikkygee <kevwedeke@gmail.com>
2021-06-24 04:32:35 -0700

    more functions and more nested loop

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * int mul - function that multiply two intergers
+ *
+ * @a: variable
+ *
+ * @b: variable
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}	
