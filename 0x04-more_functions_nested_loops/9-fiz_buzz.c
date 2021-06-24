9df9ad53e4c21ff6d833bea426e2d15e1b2804cc
mikkygee <kevwedeke@gmail.com>
2021-06-24 09:12:05 -0700

    more functions and nested loop

---
+++ 0x04-more_functions_nested_loops/9-fiz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - function that prints numbers 1 to 100
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int k;
+	for (k = 1; k < 100; k++)
+		if (k % 3 == 0)
+		{
+			printf("Fizz" );
+		}
+		else if (k % 5 == 0)
+		{
+			printf("Buzz" );
+		}
+		else if (k % 15 == 0)
+		{
+			printf("FizzBuzz" );
+		}
+		else
+		{
+			prinf("%d ", k);
+		}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}	
