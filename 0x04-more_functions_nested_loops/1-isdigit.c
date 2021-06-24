231c3bec67648c2b6c9a387e7d8f95a6bc8e890a
mikkygee <kevwedeke@gmail.com>
2021-06-24 04:10:53 -0700

    more functions and more nested loop

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,17 +5,12 @@
  *
  * @c: variable text
  *
- * Return: Always 0.
+ * Return: 1 if number is digit else 0.
  */
 
 int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
-	{
-		return (1);
-	}
-	else 
-	{
-		return (0);
-	}
+	return (1);
+	return (0);
 }
