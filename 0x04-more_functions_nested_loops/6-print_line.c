32d372ce0b2dca76d456f54103c943e9e736b5dc
mikkygee <kevwedeke@gmail.com>
2021-06-24 07:28:11 -0700

    more functions and more nested loop

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_line - function that prints a straight line
+ *
+ * @n; variable
+ *
+ * Return; Always 0.
+ */
+
+void print_line(int n);
+{
+	int k;
+
+	for (k = 0; k < n; k++)
+		_putchar('_');
+	_putchar('\n');
+}
