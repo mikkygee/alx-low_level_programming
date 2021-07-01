a321b57e8fdaa7a7d1e4dda8f778f54def368263
mikkygee <kevwedeke@gmail.com>
2021-07-01 09:55:07 -0700

    More Pointers, Arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
+
 /**
- * * _strcat - concatenates two strings
- * * @dest: first input string
- * * @src: second input string
- * *
- * * Description: appends the src string to the dest string
- * * Return: dest
- * */
+ * _strcat - concatenates two strings
+ * @dest: first input string
+ * @src: second input string
+ *
+ * Description: appends the src string to the dest string
+ * Return: dest
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int i, len;
