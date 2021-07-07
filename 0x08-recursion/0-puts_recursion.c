5266ebb2cc00fcc18398d3fe7151f290eadf187f
mikkygee <kevwedeke@gmail.com>
2021-07-07 13:05:18 -0700

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- *  * _puts_recursion - function like puts();
- *   * @s: input
- *    * Return: Always 0 (Success)
- *     */
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ **/
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
