a304a1f0e1d57e684bd391b5897b92879246dfc4
mikkygee <kevwedeke@gmail.com>
2021-07-07 13:00:49 -0700

    holberton

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,4 +1,8 @@
-on(char *s);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -7,3 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+#endif 
