a3ce3b5f64ad0a25a4f85dd32c5ef2a50db8fb57
mikkygee <kevwedeke@gmail.com>
2021-07-07 13:13:36 -0700

    power recurssion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- *  * _pow_recursion - Entry point
- *   * @x: input one
- *    * @y: input two
- *     * Return: Always 0 (Success)
- *      */
+* _pow_recursion - Entry point
+* @x: input one
+* @y: input two
+* Return: Always 0 (Success)
+*/
 
 int _pow_recursion(int x, int y)
 {
