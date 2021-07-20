75f4cb3182603b74ca5af26868b5cb7c882072a0
mikkygee <kevwedeke@gmail.com>
2021-07-20 09:12:58 -0700

    structure typeof

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,6 +8,7 @@
   *
   * Return: Nothing
   */
+
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
@@ -16,5 +17,5 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", (d->age) ? d->age : 0);
 		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	
-}
+	}
 }
