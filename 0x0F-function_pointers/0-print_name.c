f5712af5c5e0079040a7b0391c5494c58730af92
mikkygee <kevwedeke@gmail.com>
2021-07-20 12:16:00 -0700

    Function Pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: string name
+ * @f: callback
+ * Return: void
+ */
+
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "function_pointers.h"
+
+/**
+ * op_add - sum of two integers
+ * @a: num one
+ * @b: num two
+ * Return: the result (int)
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+
+/**
+ * op_sub - sub of two integers
+ * @a: num one
+ * @b: num two
+ * Return: the result (int)
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+
+/**
+ * op_mul - mul of two integers
+ * @a: num one
+ * @b: num two
+ * Return: the result (int)
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+ * op_div - div of two integers
+ * @a: num one
+ * @b: num two
+ * Return: the result (int)
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+/**
+ * op_mod - mod of two integers
+ * @a: num one
+ * @b: num two
+ * Return: the result (int)
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
